231dcda2a39fe953296c756c680bfbc8281a2207
hongsong <2574130973@qq.com>
2020-07-24 11:35:19 +0800

    hs/123456

---
+++ springcloud-project-common/src/main/java/com/aaa/staticproperties/StatisticsLevel.java
@@ -0,0 +1,18 @@
+package com.aaa.staticproperties;
+
+/**
+ * @description: StatisticsLevel
+ * @author: 彭于晏
+ * @create: 2020-07-20 21:12
+ **/
+public class StatisticsLevel {
+
+    public static final String Jialevel="甲级";
+    public static final String yilevel="乙级";
+    public static final String binglevel="丙级";
+    public static final String dinglevel="丁级";
+
+
+
+
+}
